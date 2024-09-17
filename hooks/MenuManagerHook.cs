8378a7266a1630fde7e9e44dfc9454f588e1b08a
abb2k <75845401+abb2k@users.noreply.github.com>
2024-09-17 23:41:26 +0300

    initial commit

---
+++ hooks/MenuManagerHook.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Lootrun.hooks
+{
+    [HarmonyPatch(typeof(MenuManager))]
+    internal class MenuManagerHook
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        static void StartHook(ref GameObject ___menuButtons)
+        {
+            GameObject speedlootButton = GameObject.Instantiate(___menuButtons.transform.GetChild(1).gameObject, ___menuButtons.transform);
+            speedlootButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(speedlootButton.GetComponent<RectTransform>().anchoredPosition.x, 235);
+
+            Button speedlootB = speedlootButton.GetComponent<Button>();
+            TextMeshProUGUI speedlootT = speedlootButton.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+
+            speedlootT.text = "> Lootrun";
+        }
+    }
+}
---
+++ mod.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Lootrun.hooks;
+using UnityEngine;
+
+namespace Lootrun
+{
+    [BepInPlugin(GUID, modName, modVersion)]
+    public class LootrunBase : BaseUnityPlugin
+    {
+        public const string GUID = "abb2k.Speedloot";
+        public const string modName = "Speedloot";
+        public const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(GUID);
+
+        private static LootrunBase Instance;
+
+        public static ManualLogSource mls;
+
+        void Awake()
+        {
+            if (!Instance)
+            {
+                Instance = this;
+            }
+
+            mls = Logger;
+
+            mls.LogInfo("Lootrun has loaded :D");
+
+            harmony.PatchAll(typeof(LootrunBase));
+            harmony.PatchAll(typeof(MenuManagerHook));
+        }
+    }
+}
