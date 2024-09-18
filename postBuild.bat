90d0cf38000390f9e8d3e19d8fc678fa4f03ac62
abb2k <75845401+abb2k@users.noreply.github.com>
2024-09-18 16:15:59 +0300

    fix

---
+++ postBuild.bat
@@ -0,0 +1,5 @@
+@echo off
+
+MOVE "C:\Users\Amitay\Documents\lethal company modding\Lootrun\LCSpeedlootMod\bin\Debug\Lootrun.dll" "C:\Program Files (x86)\Steam\steamapps\common\Lethal Company\BepInEx\plugins"
+
+start steam://rungameid/1966720
