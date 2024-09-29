9bfc75eae065b6087ed777afae85a42bc443087d
abb2k <75845401+abb2k@users.noreply.github.com>
2024-09-29 03:25:42 +0300

    Delete postBuild.bat

--- postBuild.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-MOVE "C:\Users\Amitay\Documents\lethal company modding\Lootrun\Lootrun\bin\Debug\netstandard2.1\Lootrun.dll" "C:\Users\Amitay\Documents\lethal company modding\Lootrun\Package\Lootrun"
-
-xcopy /s "C:\Users\Amitay\Documents\lethal company modding\Lootrun\Package" "C:\Program Files (x86)\Steam\steamapps\common\Lethal Company\BepInEx\plugins" /Y
-
-start /b C:\"Program Files (x86)"\Steam\steamapps\common\"Lethal Company"\"Lethal Company.exe"
